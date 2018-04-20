500d691cb8f497d39071bc7faf9b0e75fc7112dc
YUNSW <seongukyun17@gmail.com>
2018-04-20 12:13:12 +0200

    HelloWorld Example

---
+++ src/decoupled/HelloWorldDecoupled.java
@@ -0,0 +1,12 @@
+package decoupled;
+
+public class HelloWorldDecoupled {
+    public static void main(String... args) {
+        MessageRenderer mr = new StandardOutMessageRenderer();
+        MessageProvider mp = new HelloWorldMessageProvider();
+
+        mr.setMessageProvider(mp);
+        mr.render();
+
+    }
+}
